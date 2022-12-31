ae04cf1f86e615d12bab5d6f6681d64e5969bf80
kitravee <kitravee.swk@gmail.com>
2022-12-31 22:46:03 +0700

    fix: add user resolver

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,11 +5,12 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
@@ -17,6 +18,7 @@ async function bootstrap() {
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/graphiql`);
 }
 
 bootstrap();
